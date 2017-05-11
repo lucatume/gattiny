3e1457ebde8b22cb0d50c3b22b34a1494cfec709
Luca Tumedei <luca@theaveragedev.com>
2017-05-11 18:47:22 +0200

    refactor to use di52

--- gattiny.php
+++ gattiny.php
@@ -10,50 +10,11 @@ Text Domain: gattiny
 Domain Path: /languages
 */
 
-spl_autoload_register('gattiny_autoload');
-function gattiny_autoload($class) {
-	if (0 !== strpos($class, 'gattiny_')) {
-		return false;
-	}
-	$src = dirname(__FILE__) . '/src/';
-	$className = str_replace('gattiny_', '', $class);
-	$relativeClassPath = str_replace('_', '/', $className);
-	$path = $src . $relativeClassPath . '.php';
-	if (!file_exists($path)) {
-		return false;
-	}
+include_once dirname(__FILE__) . '/vendor/autoload_52.php';
 
-	include $path;
+$di = new tad_DI52_Container();
 
-	return true;
-}
+$di->singleton('gattiny_System', 'gattiny_System');
 
-add_action('admin_init', 'gattiny_maybeDeactivate');
-function gattiny_maybeDeactivate() {
-	$plugin = plugin_basename(__FILE__);
-	if (!empty($_GET['activate']) && is_plugin_active($plugin) && current_user_can('activate_plugins')) {
-		return;
-	}
-	if ('0' === get_option('gattiny_supported')) {
-		unset($_GET['activate']);
-		add_action('admin_notices', 'gattiny_unsupportedNotice');
-		deactivate_plugins(plugin_basename(__FILE__));
-	}
-}
-
-function gattiny_unsupportedNotice() {
-	deactivate_plugins(plugin_basename(__FILE__));
-	?>
-    <div class="notice notice-error gattiny_Notice gattiny_Notice--unsupported">
-        <p><?php _e('Gattiny is not supported by your server!', 'gattiny'); ?></p>
-    </div>
-	<?php
-}
-
-add_filter('wp_image_editors', 'gattiny_filterImageEditors');
-function gattiny_filterImageEditors(array $imageEditors) {
-	array_unshift($imageEditors, 'gattiny_GifEditor');
-
-	return $imageEditors;
-}
-
+add_action('admin_init', $di->callback('gattiny_System', 'maybeDeactivate'));
+add_filter('wp_image_editors', $di->callback('gattiny_System', 'filterImageEditors'));
---
+++ src/gattiny/System.php
@@ -0,0 +1,39 @@
+<?php
+
+class gattiny_System {
+
+	/**
+	 * @var string The path to the plugin main file.
+	 */
+	protected $mainFile;
+
+	public function __construct() {
+		$this->mainFile = dirname(dirname(dirname(__FILE__))) . '/gattiny.php';
+	}
+
+	public function maybeDeactivate() {
+		$plugin = plugin_basename($this->mainFile);
+		if (!empty($_GET['activate']) && is_plugin_active($plugin) && current_user_can('activate_plugins')) {
+			return;
+		}
+		if ('0' === get_option('gattiny_supported')) {
+			unset($_GET['activate']);
+			add_action('admin_notices', array($this, 'unsupportedNotice'));
+		}
+	}
+
+	public function unsupportedNotice() {
+		deactivate_plugins(plugin_basename($this->mainFile));
+		?>
+        <div class="notice notice-error gattiny_Notice gattiny_Notice--unsupported">
+            <p><?php _e('Gattiny is not supported by your server!', 'gattiny'); ?></p>
+        </div>
+		<?php
+	}
+
+	public function filterImageEditors(array $imageEditors) {
+		array_unshift($imageEditors, 'gattiny_GifEditor');
+
+		return $imageEditors;
+	}
+}
