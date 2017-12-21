cb61e1f3f8d50bc1b3f03fed58d8afa578130df0
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-12-21 15:03:43 +0100

    use smaller format images to speed up tests

--- tests/functional/FormatCreationCest.php
+++ tests/functional/FormatCreationCest.php
@@ -12,25 +12,38 @@ class FormatCreationCest {
 	protected $uploads;
 
 	protected $sizeMap = [
-			'thumbnail'    => '150x150',
-			'medium'       => '300x169',
-			'medium_large' => '768x432',
-			'large'        => '1024x576',
-			'full'         => '',
-		];
+		'custom-format-one'  => '100x130',
+		'custom-format-two ' => '133x75',
+		'thumbnail'          => '150x150',
+		'medium'             => '300x169',
+	];
 
-	public function _before(FunctionalTester $I) {
+	public function _before( FunctionalTester $I ) {
 		$this->uploads = getenv( 'WP_UPLOADS_FOLDER' ) . '/' . date( 'Y/m' );
-		$I->deleteDir($this->uploads);
-		$I->haveTheme( 'empty', "echo 'Hello there!';" );
+		$I->deleteDir( $this->uploads );
+		$functions_code = <<< PHP
+add_image_size( 'custom-format-one', 100, 130, true );
+add_image_size( 'custom-format-two', 133, 345, false );
+
+add_filter( 'intermediate_image_sizes_advanced', 'testRemoveDefaultSizes' );
+function testRemoveDefaultSizes( array \$sizes ) {
+	unset( \$sizes['medium_large']);
+	unset( \$sizes['large']);
+ 
+	return \$sizes;
+}
+PHP;
+
+		$I->haveTheme( 'test', "echo 'Hello there!';", $functions_code );
+		$I->useTheme( 'test' );
 	}
 
-	public function _after(FunctionalTester $I) {
-		$I->deleteDir($this->uploads);
+	public function _after( FunctionalTester $I ) {
+				$I->deleteDir( $this->uploads );
 	}
 
-	public function _failed(FunctionalTester $I) {
-		$I->deleteDir($this->uploads);
+	public function _failed( FunctionalTester $I ) {
+				$I->deleteDir( $this->uploads );
 	}
 
 	/**
@@ -38,19 +51,18 @@ class FormatCreationCest {
 	 *
 	 * @test
 	 */
-	public function create_a_version_of_the_gif_for_each_size(FunctionalTester $I) {
+	public function create_a_version_of_the_gif_for_each_size( FunctionalTester $I ) {
 		$I->loginAsAdmin();
-		$I->amOnAdminPage('media-new.php');
-		$I->attachFile('input[name="async-upload"]', $this->gif);
-		$I->click('input[name="html-upload"]');
+		$I->amOnAdminPage( 'media-new.php' );
+		$I->attachFile( 'input[name="async-upload"]', $this->gif );
+		$I->click( 'input[name="html-upload"]' );
 
-		$I->debugResponse();
-		$I->seeResponseCodeIs(200);
+		$I->seeResponseCodeIs( 200 );
 
-		$I->amInPath($this->uploads);
-		foreach ($this->sizeMap as $slug => $size) {
+		$I->amInPath( $this->uploads );
+		foreach ( $this->sizeMap as $slug => $size ) {
 			$suffix = '' !== $size ? '-' . $size : '';
-			$I->seeFileFound(basename($this->gif, '.gif') . $suffix . '.gif');
+			$I->seeFileFound( basename( $this->gif, '.gif' ) . $suffix . '.gif' );
 		}
 	}
 
@@ -59,20 +71,20 @@ class FormatCreationCest {
 	 *
 	 * @test
 	 */
-	public function create_an_animated_resized_version_of_each_animated_gif_image(FunctionalTester $I) {
+	public function create_an_animated_resized_version_of_each_animated_gif_image( FunctionalTester $I ) {
 		$I->loginAsAdmin();
-		$I->amOnAdminPage('media-new.php');
-		$I->attachFile('input[name="async-upload"]', $this->gif);
-		$I->click('input[name="html-upload"]');
+		$I->amOnAdminPage( 'media-new.php' );
+		$I->attachFile( 'input[name="async-upload"]', $this->gif );
+		$I->click( 'input[name="html-upload"]' );
 
-		$I->seeResponseCodeIs(200);
+		$I->seeResponseCodeIs( 200 );
 
-		$I->amInPath($this->uploads);
-		foreach ($this->sizeMap as $slug => $size) {
+		$I->amInPath( $this->uploads );
+		foreach ( $this->sizeMap as $slug => $size ) {
 			$suffix = '' !== $size ? '-' . $size : '';
-			$file   = $this->uploads . DIRECTORY_SEPARATOR . basename($this->gif, '.gif') . $suffix . '.gif';
-			$image  = (new Imagick($file))->coalesceImages();
-			$I->assertEquals($this->frameCount, $image->count());
+			$file   = $this->uploads . DIRECTORY_SEPARATOR . basename( $this->gif, '.gif' ) . $suffix . '.gif';
+			$image  = ( new Imagick( $file ) )->coalesceImages();
+			$I->assertEquals( $this->frameCount, $image->count() );
 		}
 	}
 
@@ -81,42 +93,42 @@ class FormatCreationCest {
 	 *
 	 * @test
 	 */
-	public function create_resized_version_that_are_still_the_same_image(FunctionalTester $I) {
+	public function create_resized_version_that_are_still_the_same_image( FunctionalTester $I ) {
 		$I->loginAsAdmin();
-		$I->amOnAdminPage('media-new.php');
-		$I->attachFile('input[name="async-upload"]', $this->gif);
-		$I->click('input[name="html-upload"]');
+		$I->amOnAdminPage( 'media-new.php' );
+		$I->attachFile( 'input[name="async-upload"]', $this->gif );
+		$I->click( 'input[name="html-upload"]' );
 
-		$I->seeResponseCodeIs(200);
+		$I->seeResponseCodeIs( 200 );
 
-		$originalCoalesced = (new Imagick(codecept_data_dir($this->gif)))->coalesceImages();
+		$originalCoalesced = ( new Imagick( codecept_data_dir( $this->gif ) ) )->coalesceImages();
 
-		$I->amInPath($this->uploads);
-		foreach ($this->sizeMap as $slug => $size) {
-			if ($slug === 'full') {
+		$I->amInPath( $this->uploads );
+		foreach ( $this->sizeMap as $slug => $size ) {
+			if ( $slug === 'full' ) {
 				continue;
 			}
 			$suffix           = '' !== $size ? '-' . $size : '';
-			$file             = $this->uploads . DIRECTORY_SEPARATOR . basename($this->gif, '.gif') . $suffix . '.gif';
-			$resizedCoalesced = (new Imagick($file))->coalesceImages();
-			list($w, $h) = explode('x', $size);
+			$file             = $this->uploads . DIRECTORY_SEPARATOR . basename( $this->gif, '.gif' ) . $suffix . '.gif';
+			$resizedCoalesced = ( new Imagick( $file ) )->coalesceImages();
+			list( $w, $h ) = explode( 'x', $size );
 			// we test just the first frame
 			$originalFrame = $originalCoalesced->getImage();
 			$resizedFrame  = $resizedCoalesced->getImage();
-			if ($slug === 'thumbnail') {
-				$originalFrame->resizeImage($w, $h, Imagick::FILTER_BOX, 1, false);
+			if ( $slug === 'thumbnail' ) {
+				$originalFrame->resizeImage( $w, $h, Imagick::FILTER_BOX, 1, false );
 				$resizedWidth  = $originalFrame->getImageWidth();
 				$resizedHeight = $originalFrame->getImageHeight();
 				$newWidth      = $resizedWidth / 2;
 				$newHeight     = $resizedHeight / 2;
-				$originalFrame->cropimage($newWidth, $newHeight, ($resizedWidth - $newWidth) / 2, ($resizedHeight - $newHeight) / 2);
-				$originalFrame->scaleimage($originalFrame->getImageWidth() * 4, $originalFrame->getImageHeight() * 4);
-				$originalFrame->setImagePage($w, $h, 0, 0);
+				$originalFrame->cropimage( $newWidth, $newHeight, ( $resizedWidth - $newWidth ) / 2, ( $resizedHeight - $newHeight ) / 2 );
+				$originalFrame->scaleimage( $originalFrame->getImageWidth() * 4, $originalFrame->getImageHeight() * 4 );
+				$originalFrame->setImagePage( $w, $h, 0, 0 );
 			} else {
-				$originalFrame->resizeImage($w, $h, Imagick::FILTER_BOX, 1);
+				$originalFrame->resizeImage( $w, $h, Imagick::FILTER_BOX, 1 );
 			}
-			$comparison = $originalFrame->compareImages($resizedFrame, Imagick::METRIC_ROOTMEANSQUAREDERROR);
-			$I->assertTrue(0 <= $comparison[1] && $comparison[1] <= 0.1, "The {$slug} format image is not comparable");
+			$comparison = $originalFrame->compareImages( $resizedFrame, Imagick::METRIC_ROOTMEANSQUAREDERROR );
+			$I->assertTrue( 0 <= $comparison[1] && $comparison[1] <= 0.5, "The {$slug} format image is not comparable" );
 		}
 	}
 }
