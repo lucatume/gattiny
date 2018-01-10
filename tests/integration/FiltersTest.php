acd18424e43c50a46f9cfc923ac431bdfdb6095f
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-01-10 15:59:57 +0100

    clean up after the test

--- tests/integration/FiltersTest.php
+++ tests/integration/FiltersTest.php
@@ -2,6 +2,8 @@
 
 class FiltersTest extends \Codeception\TestCase\WPTestCase {
 
+	protected $toRemove = [];
+
 	/**
 	 * It should allow filtering the image_make_intermediate_size filter
 	 *
@@ -9,20 +11,23 @@ class FiltersTest extends \Codeception\TestCase\WPTestCase {
 	 */
 	public function it_should_allow_filtering_the_image_make_intermediate_size_filter() {
 		$filtered = [];
-		add_filter('image_make_intermediate_size', function ($filename) use (&$filtered) {
+		add_filter( 'image_make_intermediate_size', function ( $filename ) use ( &$filtered ) {
 			$filtered[] = $filename;
 
 			return $filename;
-		});
+		} );
 
-		$editor = new gattiny_GifEditor(codecept_data_dir('images/kitten-animated.gif'));
+		$editor = new gattiny_GifEditor( codecept_data_dir( 'images/medium.gif' ) );
 		$editor->load();
-		$editor->save();
+		$saved = $editor->save();
+		if ( is_array( $saved ) && ! empty( $saved['path'] ) ) {
+			$this->toRemove[] = $saved['path'];
+		}
 
-		$this->assertNotEmpty($filtered);
-		$this->assertCount(1,$filtered);
-		foreach ($filtered as $filename) {
-			$this->assertRegExp('/^.*?kitten-animated.*?\.gif/', $filename);
+		$this->assertNotEmpty( $filtered );
+		$this->assertCount( 1, $filtered );
+		foreach ( $filtered as $filename ) {
+			$this->assertRegExp( '/^.*?medium.*?\.gif/', $filename );
 		}
 	}
 
@@ -32,11 +37,19 @@ class FiltersTest extends \Codeception\TestCase\WPTestCase {
 	 * @test
 	 */
 	public function it_should_not_filter_the_image_editors_if_the_plugin_is_not_supported() {
-		update_option('gattiny_supported', '0');
+		update_option( 'gattiny_supported', '0' );
 
 		$imageEditors = new gattiny_ImageEditors();
-		$filtered = $imageEditors->filterImageEditors([]);
+		$filtered     = $imageEditors->filterImageEditors( [] );
 
-		$this->assertNotContains('gattiny_GifEditor', $filtered);
+		$this->assertNotContains( 'gattiny_GifEditor', $filtered );
+	}
+
+	protected function _after() {
+		foreach ( $this->toRemove as $file ) {
+			if ( file_exists( $file ) ) {
+				unlink( $file );
+			}
+		}
 	}
 }
