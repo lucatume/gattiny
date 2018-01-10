17b0a9a06a3e039f0efecadc0b171687b272e98f
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-01-10 14:37:06 +0100

    code style

--- src/gattiny/ImageEditors.php
+++ src/gattiny/ImageEditors.php
@@ -6,12 +6,13 @@
  * Manages the image editors provided by the plugin.
  */
 class gattiny_ImageEditors {
-	public function filterImageEditors(array $imageEditors) {
-		if ('0' === get_option('gattiny_supported') || !extension_loaded('imagick')) {
+
+	public function filterImageEditors( array $imageEditors ) {
+		if ( '0' === get_option( 'gattiny_supported' ) || ! extension_loaded( 'imagick' ) ) {
 			return $imageEditors;
 		}
 
-		array_unshift($imageEditors, 'gattiny_GifEditor');
+		array_unshift( $imageEditors, 'gattiny_GifEditor' );
 
 		return $imageEditors;
 	}
