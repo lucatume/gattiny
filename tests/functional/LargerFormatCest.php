c7859c46acb8a2b85833bd93c20a746b3535af8b
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-01-13 00:12:20 +0100

    Update LargerFormatCest.php

--- tests/functional/LargerFormatCest.php
+++ tests/functional/LargerFormatCest.php
@@ -5,39 +5,11 @@ use Codeception\Exception\ModuleException;
 
 class LargerFormatCest {
 
-	protected $gif = 'images/small.gif';
 	protected $uploads;
 
 	public function _before( FunctionalTester $I ) {
 		$this->uploads = getenv( 'WP_UPLOADS_FOLDER' ) . '/' . date( 'Y/m' );
 		$I->deleteDir( $this->uploads );
-
-		$functionsCode = <<< PHP
-add_image_size( 'smaller-not-cropping', 50, 50, false );
-add_image_size( 'smaller-cropping', 55, 55, true );
-add_image_size( 'larger-not-cropping', 65, 65, false );
-add_image_size( 'larger-cropping', 70, 70, true );
-
-add_filter( 'intermediate_image_sizes_advanced', 'testRemoveDefaultSizes' );
-function testRemoveDefaultSizes( array \$sizes ) {
-	unset( \$sizes['thumbnail']);
-	unset( \$sizes['medium']);
-	unset( \$sizes['medium_large']);
-	unset( \$sizes['large']);
- 
-	return \$sizes;
-}
-PHP;
-
-		$id        = uniqid( 'test', true );
-		$themeName = "test-theme-{$id}";
-
-		try {
-			$I->haveTheme( $themeName, "echo 'Hello there!';", $functionsCode );
-		} catch ( ModuleException $e ) {
-			$I->fail( "It was not possible to have test theme; issue is {$e->getMessage()}" );
-		}
-		$I->useTheme( $themeName );
 	}
 
 	public function _after( FunctionalTester $I ) {
@@ -46,26 +18,6 @@ PHP;
 
 	public function _failed( FunctionalTester $I ) {
 		$I->deleteDir( $this->uploads );
-	}
-
-	/**
-	 * It should not create larger versions of the image
-	 *
-	 * @test
-	 */
-	public function should_not_create_larger_versions_of_the_image( FunctionalTester $I ) {
-		$I->loginAsAdmin();
-		$I->amOnAdminPage( 'media-new.php' );
-		$I->attachFile( 'input[name="async-upload"]', $this->gif );
-		$I->click( 'input[name="html-upload"]' );
-
-		$I->seeResponseCodeIs( 200 );
-
-		$I->amInPath( $this->uploads );
-		$I->seeFileFound( basename( $this->gif, '.gif' ) . '-50x50' . '.gif', $this->uploads );
-		$I->seeFileFound( basename( $this->gif, '.gif' ) . '-55x55' . '.gif', $this->uploads );
-		$I->dontSeeFileFound( basename( $this->gif, '.gif' ) . '-65x65' . '.gif', $this->uploads );
-		$I->dontSeeFileFound( basename( $this->gif, '.gif' ) . '-70x70' . '.gif', $this->uploads );
 	}
 
 	/**
@@ -93,6 +45,7 @@ PHP;
 
 		$id        = uniqid( 'test', true );
 		$themeName = "test-theme-{$id}";
+		$image = 'images/large.gif';
 
 		try {
 			$I->haveTheme( $themeName, "echo 'Hello there!';", $functionsCode );
@@ -105,7 +58,6 @@ PHP;
 
 		$I->loginAsAdmin();
 		$I->amOnAdminPage( 'media-new.php' );
-		$image = 'images/large.gif';
 		$I->attachFile( 'input[name="async-upload"]', $image );
 		$I->click( 'input[name="html-upload"]' );
 
