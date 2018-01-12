ab7d59531cf43b5a0a4379f18e407b3e2efa4405
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-01-12 19:36:07 +0100

    add upper limit to resize while keeping ratio

--- tests/functional/LargerFormatCest.php
+++ tests/functional/LargerFormatCest.php
@@ -67,4 +67,55 @@ PHP;
 		$I->dontSeeFileFound( basename( $this->gif, '.gif' ) . '-65x65' . '.gif', $this->uploads );
 		$I->dontSeeFileFound( basename( $this->gif, '.gif' ) . '-70x70' . '.gif', $this->uploads );
 	}
+
+	/**
+	 * It should create upper bound images keeping ratio
+	 *
+	 * @test
+	 */
+	public function should_create_upper_bound_images_keeping_ratio(FunctionalTester $I) {
+		$functionsCode = <<< PHP
+add_image_size( 'smaller-not-cropping', 200, 200, false );
+add_image_size( 'smaller-cropping', 200, 200, true );
+add_image_size( 'larger-not-cropping', 1000, 1000, false );
+add_image_size( 'larger-cropping', 1000, 1000, true );
+
+add_filter( 'intermediate_image_sizes_advanced', 'testRemoveDefaultSizes' );
+function testRemoveDefaultSizes( array \$sizes ) {
+	unset( \$sizes['thumbnail']);
+	unset( \$sizes['medium']);
+	unset( \$sizes['medium_large']);
+	unset( \$sizes['large']);
+ 
+	return \$sizes;
+}
+PHP;
+
+		$id        = uniqid( 'test', true );
+		$themeName = "test-theme-{$id}";
+
+		try {
+			$I->haveTheme( $themeName, "echo 'Hello there!';", $functionsCode );
+		} catch ( ModuleException $e ) {
+			$I->fail( "It was not possible to have test theme; issue is {$e->getMessage()}" );
+		}
+		$I->useTheme( $themeName );
+
+		$I->haveOptionInDatabase('gattiny-image-upper-bound', '300');
+
+		$I->loginAsAdmin();
+		$I->amOnAdminPage( 'media-new.php' );
+		$image = 'images/large.gif';
+		$I->attachFile( 'input[name="async-upload"]', $image );
+		$I->click( 'input[name="html-upload"]' );
+
+		$I->seeResponseCodeIs( 200 );
+
+		$I->amInPath( $this->uploads );
+		$I->seeFileFound( basename( $image, '.gif' ) . '-200x125' . '.gif', $this->uploads );
+		$I->seeFileFound( basename( $image, '.gif' ) . '-200x200' . '.gif', $this->uploads );
+		$I->seeFileFound( basename( $image, '.gif' ) . '-300x188' . '.gif', $this->uploads );
+		$I->seeFileFound( basename( $image, '.gif' ) . '-300x300' . '.gif', $this->uploads );
+		$I->seeFileFound( 'large.gif', $this->uploads );
+	}
 }
